10ab5c72b65f59f0f5e769a49ac5884ee55d7aa3
Leo <liaoyuan@gmail.com>
2016-04-29 22:13:08 +0800

    Initial release

---
+++ README.md
@@ -0,0 +1,38 @@
+RealMute: A PocketMine chat management plugin by Leo3418
+==========
+
+RealMute is a plugin that allows adminstrator to mute players in chat.(Simplified) translated version. <br />
+
+Features
+----------
+- Mute and unmute individual players in chat
+- Keep muting players even if they quit and join server again
+- Mute all players in chat
+
+Usage
+----------
+- `/rmute <player>` Mute a player
+- `/runmute <player>` Unmute a player
+- `/muteall` Mute all players
+- `/unmuteall` Unmute all players
+
+Permissions
+----------
+- `realmute` Allows all RealMute commands
+- `realmute.mute` Allows muting individual players
+- `realmute.unmute` Allows unmuting individual players
+- `realmute.muteall` Allows muting all players
+- `realmute.unmuteall` Allows unmuting individual players
+Default setting of those permissions is **OP**.
+
+License
+----------
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. <br />
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. <br />
+
+You should have received a copy of the GNU General Public License
+along with this program. [If not, click here.] (http://www.gnu.org/licenses/)
---
+++ src/RealMute/Main.php
@@ -0,0 +1,130 @@
+<?php  
+
+/* 
+	RealMute, a plugin that allows adminstrator to mute players in chat.
+	Copyright (C) 2016 Leo3418 <https://github.com/Leo3418>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+namespace RealMute;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\level\Position;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->notice("Copyright (C) 2016 Leo3418");
+		$this->getLogger()->notice("RealMute is free software licensed under GNU GPLv3 with the absence of any warranty");
+		if(!is_dir($this->getDataFolder())) @mkdir($this->getDataFolder(), 0777, true);
+		$config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+			"muteall" => false,
+		));
+	}
+	public function onDisable(){
+		$this->getConfig()->save();
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch($command->getName()){
+			case "rmute":
+				if(count($args) !== 1){
+					$sender->sendMessage("Usage: " . $command->getUsage());
+					return true;
+				}
+				$name = array_shift($args);
+				if (!$this->getConfig()->get($name.".mute")){ // might cause runtime error
+					$this->getConfig()->set($name.".mute", true);
+					$this->getConfig()->save();
+					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully muted " . $name . ".");
+					return true;
+				}
+				else{
+					$sender->sendMessage(TextFormat::RED . "[RealMute] " . $name . " has been already muted.");
+					return true;
+				}
+			case "runmute":
+				if(count($args) !== 1){
+					$sender->sendMessage("Usage: " . $command->getUsage());
+					return true;
+				}
+				$name = array_shift($args);
+				if ($this->getConfig()->get($name.".mute") == true){ // might cause runtime error
+					$this->getConfig()->remove($name.".mute", true);
+					$this->getConfig()->save();
+					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully unmuted " . $name . ".");
+					return true;
+				}
+				else{
+					$sender->sendMessage(TextFormat::RED . "[RealMute] " . $name . " is not muted.");
+					return true;
+				}
+			case "muteall":
+				if(count($args) !== 0){
+					$sender->sendMessage("Usage: " . $command->getUsage());
+					return true;
+				}
+				if ($this->getConfig()->__get("muteall") == false){
+					$this->getConfig()->set("muteall", true);
+					$this->getConfig()->save();
+					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully muted all players.");
+					return true;
+				}
+				else{
+					$sender->sendMessage(TextFormat::RED . "[RealMute] Players have been already muted.");
+					return true;
+				}
+			case "unmuteall":
+				if(count($args) !== 0){
+					$sender->sendMessage("Usage: " . $command->getUsage());
+					return true;
+				}
+				if ($this->getConfig()->__get("muteall") == true){
+					$this->getConfig()->set("muteall", false);
+					$this->getConfig()->save();
+					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully unmuted all players.");
+					return true;
+				}
+				else{
+					$sender->sendMessage(TextFormat::RED . "[RealMute] Players have been already unmuted.");
+					return true;
+				}
+		}
+	}
+	public function onPlayerChat(PlayerChatEvent $event) {
+		$player = $event->getPlayer()->getName();
+		if ($this->getConfig()->get("muteall") == true){
+			$event->setCancelled(true);
+			return true;
+		}
+		elseif($this->getConfig()->get($player.".mute") == true){ // might cause runtime error
+			$event->setCancelled(true);
+			return;
+		}
+		else return true;
+	}
+}
