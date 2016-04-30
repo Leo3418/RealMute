f4dbda0be7d93730ba9312fc897f24378ebbb2b2
Leo <liaoyuan@gmail.com>
2016-04-30 13:18:58 +0800

    Version 1.1.0, new features, code clean-up and adjustments
    Version 1.1.0
    - Send notification to muted players
    - "About"
    - Code clean-up and adjustments

--- src/RealMute/Main.php
+++ src/RealMute/Main.php
@@ -2,7 +2,7 @@
 
 /* 
 	RealMute, a plugin that allows adminstrator to mute players in chat.
-	Copyright (C) 2016 Leo3418 <https://github.com/Leo3418>
+	Copyright (C) 2016 Leo3418 (https://github.com/Leo3418)
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+	along with this program.  If not, see (http://www.gnu.org/licenses/). 
 */
 
 namespace RealMute;
@@ -44,6 +44,7 @@ class Main extends PluginBase implements Listener{
 		if(!is_dir($this->getDataFolder())) @mkdir($this->getDataFolder(), 0777, true);
 		$config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
 			"muteall" => false,
+			"notification" => false,
 		));
 	}
 	public function onDisable(){
@@ -51,13 +52,54 @@ class Main extends PluginBase implements Listener{
 	}
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
 		switch($command->getName()){
+			case "rm":
+				if(count($args) !== 1){
+					$sender->sendMessage("Usage: " . $command->getUsage());
+					return true;
+				}
+				$option = array_shift($args);
+				if($option == "help"){
+					$helpmsg  = TextFormat::AQUA . "[RealMute] Options\n";
+					$helpmsg .= TextFormat::GOLD . "/rm notify " . TextFormat::WHITE . "Toggle notification to muted players\n";
+					$helpmsg .= TextFormat::GOLD . "/rm about " . TextFormat::WHITE . "Show information about this plugin\n";
+					$sender->sendMessage($helpmsg);
+					return true;
+				}
+				if($option == "notify"){
+					if($this->getConfig()->__get("notification") == false){
+						$this->getConfig()->set("notification", true);
+						$this->getConfig()->save();
+						$sender->sendMessage(TextFormat::GREEN ."[RealMute] Muted players will be notified when they are sending messages.");
+						return true;
+					}
+					else{
+						$this->getConfig()->set("notification", false);
+						$this->getConfig()->save();
+						$sender->sendMessage(TextFormat::YELLOW ."[RealMute] Muted players will not be notified.");
+						return true;
+					}
+				}
+				if($option == "about"){
+					$aboutmsg  = TextFormat::AQUA . "RealMute Version " . $this->getDescription()->getVersion() . "\n";
+					$aboutmsg .= "RealMute is a plugin that allows adminstrator to mute players in chat.\n";
+					$aboutmsg .= "Copyright (C) 2016 Leo3418 (https://github.com/Leo3418)\n";
+					$aboutmsg .= "This is free software licensed under GNU GPLv3 with the absence of any warranty.\n";
+					$aboutmsg .= "See http://www.gnu.org/licenses/ for details.\n";
+					$aboutmsg .= "You can find updates and source code of this plugin at " . $this->getDescription()->getWebsite() . "\n";
+					$sender->sendMessage($aboutmsg);
+					return true;
+				}
+				else{
+					$sender->sendMessage("Usage: " . $command->getUsage());
+					return true;
+				}
 			case "rmute":
 				if(count($args) !== 1){
 					$sender->sendMessage("Usage: " . $command->getUsage());
 					return true;
 				}
 				$name = array_shift($args);
-				if (!$this->getConfig()->get($name.".mute")){ // might cause runtime error
+				if(!$this->getConfig()->get($name.".mute")){ 
 					$this->getConfig()->set($name.".mute", true);
 					$this->getConfig()->save();
 					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully muted " . $name . ".");
@@ -73,7 +115,7 @@ class Main extends PluginBase implements Listener{
 					return true;
 				}
 				$name = array_shift($args);
-				if ($this->getConfig()->get($name.".mute") == true){ // might cause runtime error
+				if($this->getConfig()->get($name.".mute") == true){
 					$this->getConfig()->remove($name.".mute", true);
 					$this->getConfig()->save();
 					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully unmuted " . $name . ".");
@@ -84,11 +126,11 @@ class Main extends PluginBase implements Listener{
 					return true;
 				}
 			case "muteall":
-				if(count($args) !== 0){
+				if (count($args) !== 0){
 					$sender->sendMessage("Usage: " . $command->getUsage());
 					return true;
 				}
-				if ($this->getConfig()->__get("muteall") == false){
+				if($this->getConfig()->__get("muteall") == false){
 					$this->getConfig()->set("muteall", true);
 					$this->getConfig()->save();
 					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully muted all players.");
@@ -103,7 +145,7 @@ class Main extends PluginBase implements Listener{
 					$sender->sendMessage("Usage: " . $command->getUsage());
 					return true;
 				}
-				if ($this->getConfig()->__get("muteall") == true){
+				if($this->getConfig()->__get("muteall") == true){
 					$this->getConfig()->set("muteall", false);
 					$this->getConfig()->save();
 					$sender->sendMessage(TextFormat::GREEN ."[RealMute] Successfully unmuted all players.");
@@ -117,12 +159,14 @@ class Main extends PluginBase implements Listener{
 	}
 	public function onPlayerChat(PlayerChatEvent $event) {
 		$player = $event->getPlayer()->getName();
-		if ($this->getConfig()->get("muteall") == true){
+		if($this->getConfig()->get("muteall") == true){
 			$event->setCancelled(true);
+			if($this->getConfig()->get("notification") == true) $event->getPlayer()->sendMessage(TextFormat::RED . "You have been muted in chat.");
 			return true;
 		}
-		elseif($this->getConfig()->get($player.".mute") == true){ // might cause runtime error
+		elseif($this->getConfig()->get($player.".mute") == true){
 			$event->setCancelled(true);
+			if($this->getConfig()->get("notification") == true) $event->getPlayer()->sendMessage(TextFormat::RED . "You have been muted in chat.");
 			return;
 		}
 		else return true;
