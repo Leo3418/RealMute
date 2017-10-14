af88a164704ec2b152d0ca969aa18db44b3e3afe
Otosaka Yu <proplayz05@gmail.com>
2017-10-14 15:53:53 -0600

    pmmp new api

--- src/RealMute/Main.php
+++ src/RealMute/Main.php
@@ -118,7 +118,8 @@ class Main extends PluginBase implements Listener{
 		else $userconfig->set("identity", strval($event->getPlayer()->getAddress()));
 		$userconfig->save();
 	}
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+	{
 		switch($command->getName()){
 			case "realmute":
 				if(count($args) != 1 && count($args) != 2){
@@ -537,6 +538,7 @@ class Main extends PluginBase implements Listener{
 					return true;
 				}
 		}
+		return true;
 	}
 	public function onPlayerChat(PlayerChatEvent $event){
 		$player = $event->getPlayer()->getName();
